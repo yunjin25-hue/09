130dcc19ee6fb790cd878a0374e14c3a973293e4
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-30 16:31:19 +0900

    5

--- main.c
+++ main.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4 //숫자가 무엇을 의미하는지 알려줄 수 있음, 숫자 바꿀 때도 용이 
- 
-void square_array(int a[],int size);
-void print_array(int a[],int size);
-  
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS],int B[][COLS],int C[][COLS])
+{
+     int i,j;
+     for(i=0; i<ROWS; i++)
+      for(j=0; j<COLS; j++)
+       C[i][j]=A[i][j] + B[i][j];
+}
+
+void printMatrix(int A[][COLS])
+{
+     int i,j;
+     for(i=0; i<ROWS; i++)
+     {
+        for(j=0; j<COLS; j++)
+         printf("%3d",A[i][j]);
+        printf("\n");
+     } 
+}
 
 int main(int argc, char *argv[])
 {
-  //실습 4
-  int list[SIZE]={1,2,3,4};
+  //실습 5
   
-  print_array(list,SIZE);
-  square_array(list,SIZE);
-  print_array(list,SIZE);
-    
+  int A[ROWS][COLS]={
+      {2,3,0},
+      {8,9,1},
+      {7,0,5}};
+  int B[ROWS][COLS]={
+      {1,0,0},
+      {0,1,0},
+      {0,0,1}};
+  int C[ROWS][COLS];
+  
+  addMatrix(A,B,C);
+  printMatrix(C);
+  
   system("PAUSE");	
   return 0;
 }
 
-void square_array(int a[],int size)
-{
-  int i;
-  
-  for(i=0; i<size; i++)
-   a[i]=a[i]*a[i];
-}
-
-void print_array(int a[],int size)
-{
-  int i;
-  
-  for(i=0; i<size; i++)
-   printf("%3d",a[i]);
-  printf("\n");
-}
