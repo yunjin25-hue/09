a0a3d8310b2f518f8449c32616433737419ad99d
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-30 15:50:49 +0900

    2

--- main.c
+++ main.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 3 //숫자가 무엇을 의미하는지 알려줄 수 있음, 숫자 바꿀 때도 용이  
 
 int main(int argc, char *argv[])
 {
   
-  //실습 1
+  //실습 2
   
-  //5명의 학생에 대한 시험성적 저장 배열 선언 
-   
-   int score[5];
-   int i;
-   
-   score[0]=10;
-   score[1]=20;
-   score[2]=30;
-   score[3]=40;
-   score[4]=50;
-   
-   for(i=0; i<5; i++)
-   {
-     printf("grade[%i] = %i\n",i,score[i]);
-   }
+  int i;
+  int sum=0;
+  int score[SIZE]; //컴파일 시 SIZE=5  
+  
+  //점수 입력 
+  printf("%i명 점수를 입력하세요\n",SIZE);
+  
+  for(i=0; i<SIZE; i++)
+  {
+    scanf("%d",&score[i]); 
+    sum+=score[i];
+  }
+  
+  //점수 화면 출력(반복문 활용) 
+  #if 1 //출력하고 싶으면 0-->1로 바꾸기  
+  for(i=0; i<SIZE; i++)
+   printf("grade[%i] = %i\n",i,score[i]);
+  #endif //출력 막는 기능  
+  
+  printf("성적 평균 : %d\n",sum/SIZE); 
             
-  
   system("PAUSE");	
   return 0;
 }
+
