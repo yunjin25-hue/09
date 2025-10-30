f677d83d70f435d006dcc7657651d7b398d1a646
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-30 16:10:03 +0900

    3

--- main.c
+++ main.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 3 //숫자가 무엇을 의미하는지 알려줄 수 있음, 숫자 바꿀 때도 용이  
+#define SIZE 5 //숫자가 무엇을 의미하는지 알려줄 수 있음, 숫자 바꿀 때도 용이  
 
 int main(int argc, char *argv[])
 {
   
-  //실습 2
+  //실습 3
   
+  //SIZE 크기 배열 선언  
   int i;
-  int sum=0;
-  int score[SIZE]; //컴파일 시 SIZE=5  
-  
-  //점수 입력 
-  printf("%i명 점수를 입력하세요\n",SIZE);
+  int a[SIZE]={1,2,3,4,5,};
+  int b[SIZE]={1,2,3,4,5,};
+  int flag_same=1;
   
   for(i=0; i<SIZE; i++)
   {
-    scanf("%d",&score[i]); 
-    sum+=score[i];
+    if(a[i] != b[i])
+    { 
+      printf("%i-the element is diff.\n",i);
+      flag_same=0;
+    }
   }
   
-  //점수 화면 출력(반복문 활용) 
-  #if 1 //출력하고 싶으면 0-->1로 바꾸기  
-  for(i=0; i<SIZE; i++)
-   printf("grade[%i] = %i\n",i,score[i]);
-  #endif //출력 막는 기능  
+  printf("Are a and b the same? %i\n", flag_same);     
   
-  printf("성적 평균 : %d\n",sum/SIZE); 
-            
   system("PAUSE");	
   return 0;
 }
 
+/*int i;
+  int grade[SIZE];
+  int score[SIZE]; 
+  
+  //점수 입력 
+  for(i=0; i<SIZE; i++)
+  {
+    grade[i]=rand() % 100; //0~99까지의 값만 나옴  
+  }
+  
+  for(i=0; i<SIZE; i++)
+   score[i]=grade[i]; 
+    
+  //점수 화면 출력(반복문 활용) 
+  #if 1 
+  for(i=0; i<SIZE; i++)
+   printf("grade[%i]=%i, score[%i] = %i\n",i,score[i]);
+  #endif
+  */
